99da21de7f6f23aa36905c1b5be97078bf258c7f
AlexSanLo <san.lope.alex@gmail.com>
2024-12-09 19:35:31 +0100

    Mostrar_Datos_Json

--- javascript/circuitos.js
+++ javascript/circuitos.js
@@ -1,7 +1,40 @@
-const URLcircuitos = 'https://ergast.com/api/f1/2012.json';
+const URLCircuitos = 'https://ergast.com/api/f1/2012.json';
 
-async function fetchcircuitos() {
-    const responsecircuitos = await fetch(URLcircuitos);
-    const pilotos = await responsecircuitos.json();
+async function fetchCircuitos() {
+    const responseCircuitos = await fetch(URLCircuitos);
+    const data = await responseCircuitos.json();
 
+    const circuitos = data.MRData.RaceTable.Races;
+
+    let circuitosFiltrados = [];
+
+    const container = document.querySelector('#CircuitosContainer');
+
+    circuitos.forEach(circuito => {
+        const circuitoInfo = {
+            raceName: circuito.raceName,
+            circuitName: circuito.Circuit.circuitName,
+            locality: circuito.Circuit.Location.locality,
+            country: circuito.Circuit.Location.country
+        };
+
+        const card = createCardCircuito(circuitoInfo);
+        container.appendChild(card);
+    });
 };
+
+function createCardCircuito(circuito) {
+    const plantilla = document.querySelector('#CardCircuito'); 
+    const cardClonada = plantilla.cloneNode(true);
+
+    cardClonada.style.display = 'block';
+
+    cardClonada.querySelector('#raceName').textContent = `${circuito.raceName}`;
+    cardClonada.querySelector('#circuitName').textContent = `Circuito: ${circuito.circuitName}`;
+    cardClonada.querySelector('#locality').textContent = `Localidad: ${circuito.locality}`;
+    cardClonada.querySelector('#country').textContent = `${circuito.country}`;
+
+    return cardClonada;
+}
+
+document.addEventListener('DOMContentLoaded', fetchCircuitos);
--- javascript/pilotos.js
+++ javascript/pilotos.js
@@ -2,25 +2,46 @@ const URLpilotos = 'https://ergast.com/api/f1/drivers.json';
 
 async function fetchpilotos() {
     const responsePilotos = await fetch(URLpilotos);
-    const data = await responsePilotos.json(); 
+    const data = await responsePilotos.json();
 
 
     const pilotos = data.MRData.DriverTable.Drivers;
 
     let pilotosFiltrados = [];
 
+    const container = document.querySelector('#pilotosContainer');
 
-    for (let piloto of pilotos) {
-        let pilotoInfo = {
-            driverId: piloto.driverId,
+    pilotos.forEach(piloto => {
+        const pilotoInfo = {
+            driverId: MayusPrimeraLetraId(piloto.driverId),
             givenName: piloto.givenName,
             familyName: piloto.familyName,
             dateOfBirth: piloto.dateOfBirth,
             nationality: piloto.nationality
         };
-        pilotosFiltrados.push(pilotoInfo);
-    }
 
-    console.log(pilotosFiltrados); 
+        const card = createCard(pilotoInfo);
+        container.appendChild(card);
+    });
 };
 
+function MayusPrimeraLetraId(string) { 
+    return string.charAt(0).toUpperCase() + string.slice(1); 
+}
+
+function createCard(piloto) {
+
+    const plantilla = document.querySelector('#CardPilotos');
+    const cardClonada = plantilla.cloneNode(true);
+
+    cardClonada.style.display = 'block';
+
+    cardClonada.querySelector('#idPiloto').textContent = `${piloto.driverId}`;
+    cardClonada.querySelector('#nombre').textContent = `${piloto.givenName} ${piloto.familyName}`;
+    cardClonada.querySelector('#fechaNacimiento').textContent = `Fecha Nacimiento: ${piloto.dateOfBirth}`;
+    cardClonada.querySelector('#nacionalidadCard').textContent = `Nacionalidad: ${piloto.nationality}`;
+
+    return cardClonada;
+}
+
+document.addEventListener('DOMContentLoaded', fetchpilotos);
