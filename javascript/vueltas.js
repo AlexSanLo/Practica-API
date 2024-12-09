1c250caf3edbf23035af54c5545921b945ab287d
HLancha <hugolanloz@gmail.com>
2024-12-09 11:38:38 +0100

    me voy a matar

--- javascript/vueltas.js
+++
@@ -1,8 +0,0 @@
-const URLvueltas = 'https://ergast.com/api/f1/2011/5/laps/1.json';
-
-async function fetchvueltas() {
-    const responsevueltas = await fetch(URLvueltas);
-    const pilotos = await responsevueltas.json();
-
-};
-
